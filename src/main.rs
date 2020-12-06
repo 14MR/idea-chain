e05926cc2c6b875459802c7059b01b76259c8aa3
Timur <mr14mr@yandex.ru>
2020-12-06 22:58:38 +0300

    simple register

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,31 @@ fn index() -> Option<content::Json<String>> {
     let res = serde_json::to_string(&results);
     match res {
         Ok(res) => { Some(content::Json(res)) }
-        Err(_) => {None}
+        Err(_) => { None }
+    }
+}
+
+use rocket_contrib::json::Json;
+use diesel::insert_into;
+
+#[post("/register", format = "json", data = "<user>")]
+fn register(user: Json<User>) -> Option<rocket::response::content::Json<String>> {
+    use schema::users::dsl::*;
+    let connection = establish_connection();
+
+    let res = serde_json::to_string(&user.into_inner());
+    match res {
+        Ok(res) => {
+            let u = serde_json::from_str::<User>(&res);
+
+            insert_into(users).values(&u.unwrap()).execute(&connection);
+
+            Some(content::Json(res))
+        }
+        Err(_) => { None }
     }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, register]).launch();
 }
