829f202c3221bcb1b176f71ce901e958e0e46a8f
Timur <mr14mr@yandex.ru>
2020-12-08 23:12:09 +0300

    add ui component library

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -4,8 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import {terser} from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-
-const smelte = require("smelte/rollup-plugin-smelte");
+const { preprocess } = require('./svelte.config');
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,26 +38,8 @@ export default {
         file: 'public/build/bundle.js'
     },
     plugins: [
-        smelte({
-            purge: production,
-            output: "public/global.css", // it defaults to static/global.css which is probably what you expect in Sapper
-            postcss: [], // Your PostCSS plugins
-            whitelist: [], // Array of classnames whitelisted from purging
-            whitelistPatterns: [], // Same as above, but list of regexes
-            tailwind: {
-                colors: {
-                    primary: "#b027b0",
-                    secondary: "#009688",
-                    error: "#f44336",
-                    success: "#4caf50",
-                    alert: "#ff9800",
-                    blue: "#2196f3",
-                    dark: "#212121"
-                }, // Object of colors to generate a palette from, and then all the utility classes
-                darkMode: true,
-            },
-        }),
         svelte({
+            preprocess,
             compilerOptions: {
                 // enable run-time checks when not in production
                 dev: !production
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,16 +1,13 @@
 <script>
-	import "smelte/src/tailwind.css" ;
-	import Button from "smelte/src/components/Button";
-	export let name;
+	import { MaterialApp } from 'svelte-materialify';
+	let theme = 'dark';
+	import Menu from "./components/Menu.svelte";
 </script>
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons&display=swap"
-	  rel="stylesheet" />
-<main>
-	<h6 class="mb-3 mt-6">Basic</h6>
-	<div class="py-2">
-		<Button>Button</Button>
-	</div>
-</main>
+
+
+<MaterialApp theme="{theme}">
+	<Menu/>
+</MaterialApp>
 
 <style>
 
