02df964f055f4f06805a88ae0a8b5ece9de39914
Timur <mr14mr@yandex.ru>
2020-12-08 21:45:49 +0300

    init frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import "smelte/src/tailwind.css" ;
+	import Button from "smelte/src/components/Button";
+	export let name;
+</script>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Material+Icons&display=swap"
+	  rel="stylesheet" />
+<main>
+	<h6 class="mb-3 mt-6">Basic</h6>
+	<div class="py-2">
+		<Button>Button</Button>
+	</div>
+</main>
+
+<style>
+
+</style>
