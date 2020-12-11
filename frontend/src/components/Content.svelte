cdbf17cd60fe26fe912057f25000291bde040e00
Timur <mr14mr@yandex.ru>
2020-12-11 23:11:17 +0300

    add basic layout

---
+++ frontend/src/components/Content.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {Tabs, Tab, Window, WindowItem, AppBar} from 'svelte-materialify/src';
+    import ContractList from "./ContractList.svelte";
+
+    let value = 0;
+</script>
+
+<AppBar>
+    <span slot="title"> Patent registry </span>
+    <div slot="extension">
+        <Tabs class="green-text" bind:value fixedTabs>
+            <div slot="tabs">
+                <Tab>All patents</Tab>
+                <Tab>My patents</Tab>
+            </div>
+        </Tabs>
+    </div>
+</AppBar>
+
+<Window {value} class="ma-4">
+    <WindowItem>
+        <h4>All patents</h4>
+        <div>
+            <ContractList/>
+        </div>
+    </WindowItem>
+    <WindowItem>
+        <h4>My patents</h4>
+        <ContractList/>
+    </WindowItem>
+</Window>
