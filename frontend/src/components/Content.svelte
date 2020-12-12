463a649b43551839e8f73bae7c81fadac72b3c56
Timur <mr14mr@yandex.ru>
2020-12-12 21:23:12 +0300

    add contract ui

--- frontend/src/components/Content.svelte
+++ frontend/src/components/Content.svelte
@@ -1,8 +1,28 @@
 <script>
-    import {Tabs, Tab, Window, WindowItem, AppBar} from 'svelte-materialify/src';
+    import {
+        AppBar,
+        Button,
+        Card,
+        CardActions,
+        CardText,
+        CardTitle,
+        Dialog,
+        Tab,
+        Tabs,
+        Window,
+        WindowItem
+    } from 'svelte-materialify/src';
     import PatentList from "./PatentList.svelte";
+    import Icon from 'svelte-materialify/src/components/Icon';
+    import {mdiPlus} from '@mdi/js';
+    import AddPatentForm from "./AddPatentForm.svelte";
 
     let value = 0;
+    let active = true;
+
+    function handleClick() {
+        active = !active;
+    }
 </script>
 
 <AppBar>
@@ -16,16 +36,34 @@
         </Tabs>
     </div>
 </AppBar>
-
-<Window {value} class="ma-4">
-    <WindowItem>
-        <h4>All patents</h4>
-        <div>
+<div>
+    <Window {value} class="ma-4">
+        <WindowItem>
+            <h4>All patents</h4>
+            <div>
+                <PatentList/>
+            </div>
+        </WindowItem>
+        <WindowItem>
+            <h4>My patents</h4>
             <PatentList/>
-        </div>
-    </WindowItem>
-    <WindowItem>
-        <h4>My patents</h4>
-        <PatentList/>
-    </WindowItem>
-</Window>
+        </WindowItem>
+    </Window>
+</div>
+
+<!--add button -->
+<Button id="add-btn" fab size="default" class="blue white-text" on:click={handleClick}>
+    <Icon path={mdiPlus}/>
+</Button>
+<Dialog persistent bind:active>
+    <Card>
+        <CardTitle>Add a contract</CardTitle>
+        <CardText>
+            <AddPatentForm></AddPatentForm>
+        </CardText>
+        <CardActions>
+            <Button on:click={handleClick} text>Cancel</Button>
+            <Button on:click={handleClick} text class="red-text">Save</Button>
+        </CardActions>
+    </Card>
+</Dialog>
