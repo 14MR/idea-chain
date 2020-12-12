2ce53501cee161d49876f16a00c80c037f02ea2e
Timur <mr14mr@yandex.ru>
2020-12-12 20:27:23 +0300

    add patents generation from a variable

--- frontend/src/components/PatentList.svelte
+++ frontend/src/components/PatentList.svelte
@@ -3,35 +3,26 @@
         ExpansionPanel,
     } from 'svelte-materialify/src/components/ExpansionPanels';
 
+    let patents = [
+        {
+            'title': "Some title",
+            "description": "some desc"
+        }
+    ]
+
     let value = [1];
-    let about = { index: '', active: '' };
+    let about = {index: '', active: ''};
+
     function onChange(e) {
         about = e.detail;
     }
 </script>
 
-<p class="text-center">Value: [{value}]</p>
-<p class="text-center">Index: <b>{about.index}</b>, Active: <b>{about.active}</b></p>
-
 <ExpansionPanels on:change={onChange} bind:value>
-    <ExpansionPanel>
-        <span slot="header">Item</span>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat amet natus
-        obcaecati molestiae quas mollitia error modi atque aliquam esse.
-    </ExpansionPanel>
-    <ExpansionPanel>
-        <span slot="header">Item</span>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat amet natus
-        obcaecati molestiae quas mollitia error modi atque aliquam esse.
-    </ExpansionPanel>
-    <ExpansionPanel disabled>
-        <span slot="header">Disabled</span>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat amet natus
-        obcaecati molestiae quas mollitia error modi atque aliquam esse.
-    </ExpansionPanel>
-    <ExpansionPanel>
-        <span slot="header">Item</span>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat amet natus
-        obcaecati molestiae quas mollitia error modi atque aliquam esse.
-    </ExpansionPanel>
+    {#each patents as patent}
+        <ExpansionPanel>
+            <span slot="header">{patent.title}</span>
+            {patent.description}
+        </ExpansionPanel>
+    {/each}
 </ExpansionPanels>
